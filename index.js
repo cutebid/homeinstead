ecf2ec0691135b20077101d2af1ad95deef97e56
cutebid <67562053+cutebid@users.noreply.github.com>
2022-04-22 00:36:47 +0100

    homeinstead

--- index.js
+++ index.js
@@ -6,5 +6,40 @@ window.addEventListener("scroll", function () {
     header.classList.remove("sticky");
   }
 
-  console.log(this.window.scrollY);
 });
+window.addEventListener("scroll", function () {
+  const mediaQuery = window.matchMedia("(min-width: 768px)");
+  var bottomLink = document.querySelector(".bottom-link");
+  console.log(bottomLink)
+  if (mediaQuery.matches){
+      if (this.window.scrollY < 350) {
+        bottomLink.style.display = "none";
+      } else {
+        bottomLink.style.display = "block";
+      }
+  }
+  
+
+});
+
+// let slideIndex = 0;
+// showSlides();
+
+// function showSlides() {
+//   let i;
+//   let slides = document.getElementsByClassName("mySlides");
+//   let dots = document.getElementsByClassName("dot");
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//   }
+//   slideIndex++;
+//   if (slideIndex > slides.length) {
+//     slideIndex = 1;
+//   }
+//   for (i = 0; i < dots.length; i++) {
+//     dots[i].className = dots[i].className.replace(" active", "");
+//   }
+//   slides[slideIndex - 1].style.display = "block";
+//   dots[slideIndex - 1].className += " active";
+//   setTimeout(showSlides, 5000); // Change image every 2 seconds
+// }
