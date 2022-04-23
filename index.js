f68475fd04ad2a2b61ea86f7235e99c9a724a7a0
cutebid <67562053+cutebid@users.noreply.github.com>
2022-04-23 22:38:08 +0100

    home instead

--- index.js
+++ index.js
@@ -5,41 +5,46 @@ window.addEventListener("scroll", function () {
   } else {
     header.classList.remove("sticky");
   }
-
 });
 window.addEventListener("scroll", function () {
   const mediaQuery = window.matchMedia("(min-width: 768px)");
   var bottomLink = document.querySelector(".bottom-link");
-  console.log(bottomLink)
-  if (mediaQuery.matches){
-      if (this.window.scrollY < 350) {
-        bottomLink.style.display = "none";
-      } else {
-        bottomLink.style.display = "block";
-      }
-  }
-  
 
+  if (mediaQuery.matches) {
+    if (this.window.scrollY < 350) {
+      bottomLink.style.display = "none";
+    } else {
+      bottomLink.style.display = "block";
+    }
+  }
 });
 
-// let slideIndex = 0;
-// showSlides();
 
-// function showSlides() {
-//   let i;
-//   let slides = document.getElementsByClassName("mySlides");
-//   let dots = document.getElementsByClassName("dot");
-//   for (i = 0; i < slides.length; i++) {
-//     slides[i].style.display = "none";
-//   }
-//   slideIndex++;
-//   if (slideIndex > slides.length) {
-//     slideIndex = 1;
-//   }
-//   for (i = 0; i < dots.length; i++) {
-//     dots[i].className = dots[i].className.replace(" active", "");
-//   }
-//   slides[slideIndex - 1].style.display = "block";
-//   dots[slideIndex - 1].className += " active";
-//   setTimeout(showSlides, 5000); // Change image every 2 seconds
-// }
+const hamburger = document.querySelector(".hamburger");
+let menuOpen = false;
+let menu = document.querySelector(".sidebar");
+let closeBtn = document.querySelector(".close-btn")
+
+hamburger.addEventListener("click", () => {
+  if (!menuOpen) {
+    menuOpen = true;
+    menu.classList.toggle('Nav')
+   
+  } else {
+    menuOpen = false;
+    menu.classList.remove("Nav");
+  }
+});
+
+closeBtn.addEventListener("click", () => {
+
+  if (menuOpen) {
+    menuOpen = false;
+    menu.classList.remove("Nav");
+  }
+  // else {
+  //   menuOpen = false;
+  //   menu.classList.remove("Nav");
+  // }
+});
+
